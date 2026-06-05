c9c7c5fd5aada4b3032c3d4c404a425bdab52606
九玖酒柩 <1398784304@qq.com>
2026-06-05 18:01:49 +0800

    Read today record on home page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,27 @@
+var storage = require('../../utils/storage');
+
 Page({
   data: {
-    cards: [
-      { title: '今日记录', desc: '记录饮水、运动、学习、饮食和睡眠' },
-      { title: 'To Do List', desc: '管理今天要完成的任务' },
-      { title: '历史记录', desc: '查看过去每天的记录' }
-    ]
+    today: '',
+    record: null,
+    todoSummary: '0 / 0'
+  },
+
+  onShow: function() {
+    this.loadTodayRecord();
+  },
+
+  loadTodayRecord: function() {
+    var today = storage.getTodayDate();
+    var record = storage.getTodayRecord();
+    var doneCount = record.todos.filter(function(todo) {
+      return todo.done;
+    }).length;
+
+    this.setData({
+      today: today,
+      record: record,
+      todoSummary: doneCount + ' / ' + record.todos.length
+    });
   }
 });
