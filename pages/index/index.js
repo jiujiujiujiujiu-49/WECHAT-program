8944ce2020139da898fe576eefb3ae3f253070e0
九玖酒柩 <1398784304@qq.com>
2026-06-05 17:46:25 +0800

    Simplify index placeholder

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,45 +1,9 @@
-var dateUtils = require('../../utils/date');
-var storage = require('../../utils/storage');
-
 Page({
   data: {
-    today: '',
-    record: null,
-    waterPercent: 0,
-    todoSummary: '0 / 0'
-  },
-
-  onShow: function() {
-    this.loadToday();
-  },
-
-  loadToday: function() {
-    var today = dateUtils.formatDate();
-    var record = storage.getRecordByDate(today);
-    var doneCount = record.todos.filter(function(todo) {
-      return todo.done;
-    }).length;
-    var waterPercent = record.water.target > 0
-      ? Math.min(100, Math.round((record.water.amount / record.water.target) * 100))
-      : 0;
-
-    this.setData({
-      today: today,
-      record: record,
-      waterPercent: waterPercent,
-      todoSummary: doneCount + ' / ' + record.todos.length
-    });
-  },
-
-  goRecord: function() {
-    wx.switchTab({ url: '/pages/record/record' });
-  },
-
-  goTodo: function() {
-    wx.switchTab({ url: '/pages/todo/todo' });
-  },
-
-  goHistory: function() {
-    wx.switchTab({ url: '/pages/history/history' });
+    cards: [
+      { title: '今日记录', desc: '记录饮水、运动、学习、饮食和睡眠' },
+      { title: 'To Do List', desc: '管理今天要完成的任务' },
+      { title: '历史记录', desc: '查看过去每天的记录' }
+    ]
   }
 });
