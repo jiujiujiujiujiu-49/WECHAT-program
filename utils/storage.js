c07cb97285f14554fae19d666e3e74d2ff06b45f
九玖酒柩 <1398784304@qq.com>
2026-06-05 18:01:47 +0800

    Add daily local storage module

--- utils/storage.js
+++ utils/storage.js
@@ -1,45 +1,75 @@
-var recordUtils = require('./record');
-
-var STORAGE_KEY = 'DAILY_RECORDS_V1';
-
-function readRecordMap() {
-  try {
-    return wx.getStorageSync(STORAGE_KEY) || {};
-  } catch (error) {
-    return {};
-  }
+function pad(value) {
+  return value < 10 ? '0' + value : '' + value;
 }
 
-function writeRecordMap(recordMap) {
-  wx.setStorageSync(STORAGE_KEY, recordMap || {});
+function getTodayDate() {
+  var now = new Date();
+  return now.getFullYear() + '-' + pad(now.getMonth() + 1) + '-' + pad(now.getDate());
+}
+
+function getStorageKey(date) {
+  return 'daily-record-' + date;
+}
+
+function createDefaultRecord(date) {
+  return {
+    date: date,
+    water: { amount: 0, target: 2000 },
+    workout: { checked: false, type: '', duration: 0 },
+    study: { checked: false, content: '', duration: 0 },
+    meals: { breakfast: '', lunch: '', dinner: '', snack: '' },
+    sleep: { hours: 0, quality: '' },
+    todos: []
+  };
+}
+
+function mergeWithDefaultRecord(date, record) {
+  var defaults = createDefaultRecord(date);
+  var source = record || {};
+
+  return {
+    date: date,
+    water: Object.assign({}, defaults.water, source.water || {}),
+    workout: Object.assign({}, defaults.workout, source.workout || {}),
+    study: Object.assign({}, defaults.study, source.study || {}),
+    meals: Object.assign({}, defaults.meals, source.meals || {}),
+    sleep: Object.assign({}, defaults.sleep, source.sleep || {}),
+    todos: Array.isArray(source.todos) ? source.todos : []
+  };
 }
 
 function getRecordByDate(date) {
-  var recordMap = readRecordMap();
-  return recordUtils.mergeWithDefaultRecord(date, recordMap[date]);
+  var key = getStorageKey(date);
+  var savedRecord = wx.getStorageSync(key);
+
+  if (!savedRecord) {
+    var defaultRecord = createDefaultRecord(date);
+    wx.setStorageSync(key, defaultRecord);
+    return defaultRecord;
+  }
+
+  return mergeWithDefaultRecord(date, savedRecord);
 }
 
-function saveRecord(record) {
-  var date = record.date;
-  var recordMap = readRecordMap();
-  recordMap[date] = recordUtils.mergeWithDefaultRecord(date, record);
-  writeRecordMap(recordMap);
-  return recordMap[date];
+function saveRecordByDate(date, record) {
+  var nextRecord = mergeWithDefaultRecord(date, record);
+  wx.setStorageSync(getStorageKey(date), nextRecord);
+  return nextRecord;
 }
 
-function getAllRecords() {
-  var recordMap = readRecordMap();
-  return Object.keys(recordMap)
-    .sort()
-    .reverse()
-    .map(function(date) {
-      return recordUtils.mergeWithDefaultRecord(date, recordMap[date]);
-    });
+function getTodayRecord() {
+  return getRecordByDate(getTodayDate());
+}
+
+function saveTodayRecord(record) {
+  return saveRecordByDate(getTodayDate(), record);
 }
 
 module.exports = {
-  STORAGE_KEY: STORAGE_KEY,
+  getTodayDate: getTodayDate,
   getRecordByDate: getRecordByDate,
-  saveRecord: saveRecord,
-  getAllRecords: getAllRecords
+  saveRecordByDate: saveRecordByDate,
+  getTodayRecord: getTodayRecord,
+  saveTodayRecord: saveTodayRecord,
+  createDefaultRecord: createDefaultRecord
 };
