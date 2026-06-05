48eefc4f312e1782639301e70d7d5be49dd6cbdd
九玖酒柩 <1398784304@qq.com>
2026-06-05 22:17:13 +0800

    Implement today record page logic

--- pages/today/today.js
+++ pages/today/today.js
@@ -1,11 +1,67 @@
+var storage = require('../../utils/storage');
+
+var qualityOptions = ['好', '一般', '差'];
+
 Page({
   data: {
-    sections: [
-      '饮水记录',
-      '健身打卡',
-      '学习打卡',
-      '饮食记录',
-      '睡眠记录'
-    ]
+    record: null,
+    qualityOptions: qualityOptions,
+    qualityIndex: 0
+  },
+
+  onLoad: function() {
+    this.loadRecord();
+  },
+
+  loadRecord: function() {
+    var record = storage.getTodayRecord();
+    var qualityIndex = qualityOptions.indexOf(record.sleep.quality);
+
+    this.setData({
+      record: record,
+      qualityIndex: qualityIndex >= 0 ? qualityIndex : 0
+    });
+  },
+
+  updateField: function(event) {
+    var path = event.currentTarget.dataset.path;
+    this.setData({
+      ['record.' + path]: event.detail.value
+    });
+  },
+
+  updateNumber: function(event) {
+    var path = event.currentTarget.dataset.path;
+    var value = Number(event.detail.value);
+
+    this.setData({
+      ['record.' + path]: isNaN(value) ? 0 : value
+    });
+  },
+
+  updateSwitch: function(event) {
+    var path = event.currentTarget.dataset.path;
+
+    this.setData({
+      ['record.' + path]: event.detail.value
+    });
+  },
+
+  updateQuality: function(event) {
+    var index = Number(event.detail.value);
+
+    this.setData({
+      qualityIndex: index,
+      'record.sleep.quality': qualityOptions[index]
+    });
+  },
+
+  saveRecord: function() {
+    storage.saveTodayRecord(this.data.record);
+
+    wx.showToast({
+      title: '已保存',
+      icon: 'success'
+    });
   }
 });
